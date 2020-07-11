2d4db11cc223e3aa9a3c965af3decb2908c4fc4b
Nico Arato <nico.arato@gmail.com>
2020-07-11 01:20:26 -0300

    NotiApp commit 3.
    Abrir browser en dispositivo. Plugin - In App Browser (Ionic framework).
    Action-sheet.

--- NotiApp/src/app/app.module.ts
+++ NotiApp/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -22,6 +24,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser ,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- NotiApp/src/app/components/noticia/noticia.component.ts
+++ NotiApp/src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -10,8 +13,47 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia: Article;
   @Input() indice: number;
-  constructor() { }
+  constructor( private iab: InAppBrowser,
+                private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {}
+
+  abrirNoticia() {
+
+    console.log(this.noticia.url);
+    const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+
+      
+      buttons: [
+        {
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorito');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        cssClass: ['action-dark', 'cancelar'],
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 
 }
